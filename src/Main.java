27098ea8fa477361c5c083f591c4646f843e1ad1
Yulia <seminajlj@rambler.ru>
2021-03-25 21:06:25 +0300

    Выполнение задачи

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        float cost_ticket =2517;
+        float cost_mile = 20;
+        int miles;
+        miles=(int)Math.floor(cost_ticket/cost_mile);
+        System.out.println(miles);
+    }
+}
